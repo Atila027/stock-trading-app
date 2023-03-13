114ffe2b7fec78e8cc7acfc23f647d7c6d72db28
Atila027 <atila423027@outlook.com>
2023-03-13 23:15:59 +0900

    added bootstrap module

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+        <div className='col-md-8'>
+            <h4 className='text-center'>Tradingview</h4>
+        </div>
+        <div className='col-md-4'>
+            <h4 className='text-center'>Setting</h4>
+        </div>
+      </div>
     </div>
   );
 }
