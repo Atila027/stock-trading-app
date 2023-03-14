1e093b2e4b1001f28bbde888f8f2110afcf40cd3
Atila027 <atila423027@outlook.com>
2023-03-14 22:48:07 +0900

    added feature to draw chart

--- src/App.js
+++ src/App.js
@@ -24,10 +24,23 @@ function App() {
     }
   }, [])
 
+  const initialData = [
+    { time: '2018-12-22', value: 32.51 },
+    { time: '2018-12-23', value: 31.11 },
+    { time: '2018-12-24', value: 27.02 },
+    { time: '2018-12-25', value: 27.32 },
+    { time: '2018-12-26', value: 25.17 },
+    { time: '2018-12-27', value: 28.89 },
+    { time: '2018-12-28', value: 25.46 },
+    { time: '2018-12-29', value: 23.92 },
+    { time: '2018-12-30', value: 22.68 },
+    { time: '2018-12-31', value: 22.67 },
+  ];
+
   const connectToIB = async() =>{
     let api = new Client({
       host: '127.0.0.1',
-      port: 7497,
+      port: 4001,
     });
   
     let time = await api.getCurrentTime();
@@ -76,7 +89,9 @@ function App() {
                 <button className='btn btn-primary' onClick={getStockPriceData}>Search</button>
               </div>
             </div>
-            <TradingViewChart/>
+            <div className="w-100 mt-3">
+              <TradingViewChart data={initialData}></TradingViewChart>
+            </div>
         </div>
         <div className='col-md-4'>
             <h4 className='text-center'>Setting</h4>
--- src/components/tradingviewchart.js
+++ src/components/tradingviewchart.js
@@ -1,15 +1,56 @@
-import React from "react";
 
-const TradingViewChart = () =>{
+import { createChart, ColorType } from 'lightweight-charts';
+import React, { useEffect, useRef } from 'react';
+
+const TradingViewChart = (priceData) => {
+
+  
+  const backgroundColor = 'white';
+  const lineColor = '#2962FF';
+  const textColor = 'black';
+  const areaTopColor = '#2962FF';
+  const areaBottomColor = 'rgba(41, 98, 255, 0.28)';
 
 
+  const chartContainerRef = useRef();
+  
+  useEffect(
+    () => {
 
-  return(
-    <div>
-      <h1>HI</h1>
-    </div>
-  )
+      console.log("props data", priceData);
+      const handleResize = () => {
+        chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+      };
 
-}
+      const chart = createChart(chartContainerRef.current, {
+        layout: {
+          background: { type: ColorType.Solid, color: backgroundColor },
+          textColor,
+        },
+        width: chartContainerRef.current.clientWidth,
+        height: 300,
+      });
+      chart.timeScale().fitContent();
+
+      const newSeries = chart.addAreaSeries({ lineColor, topColor: areaTopColor, bottomColor: areaBottomColor });
+      newSeries.setData(priceData.data);
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+
+        chart.remove();
+      };
+    },
+    [priceData.data, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
+  );
+
+  return (
+    <div
+      ref={chartContainerRef}
+    />
+  );
+};
 
 export default TradingViewChart
