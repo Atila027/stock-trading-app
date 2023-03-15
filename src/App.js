f060c2da1b8cc8326f0bf2fcd283c7c382aec336
Atila027 <atila423027@outlook.com>
2023-03-15 15:20:55 +0900

    updated feature that draw trading signal

--- src/App.js
+++ src/App.js
@@ -4,47 +4,38 @@ import { useState,useEffect } from 'react';
 import axios from 'axios';
 import { Client } from 'ib-tws-api';
 
-
 import TradingViewChart from './components/tradingviewchart';
 function App() {
 
-
-  useEffect(() => {
-
-    return () => {
-      
-    }
-  }, [])
-
-    const initialData =  [
-      { time: '2018-10-19', open: 180.34, high: 180.99, low: 178.57, close: 179.85 },
-      { time: '2018-10-22', open: 180.82, high: 181.40, low: 177.56, close: 178.75 },
-      { time: '2018-10-23', open: 175.77, high: 179.49, low: 175.44, close: 178.53 },
-      { time: '2018-10-24', open: 178.58, high: 182.37, low: 176.31, close: 176.97 },
-      { time: '2018-10-25', open: 177.52, high: 180.50, low: 176.83, close: 179.07 },
-      { time: '2018-10-26', open: 176.88, high: 177.34, low: 170.91, close: 172.23 },
-      { time: '2018-10-29', open: 173.74, high: 175.99, low: 170.95, close: 173.20 },
-      { time: '2018-10-30', open: 173.16, high: 176.43, low: 172.64, close: 176.24 },
-      { time: '2018-10-31', open: 177.98, high: 178.85, low: 175.59, close: 175.88 },
-      { time: '2018-11-01', open: 176.84, high: 180.86, low: 175.90, close: 180.46 },
-      { time: '2018-11-02', open: 182.47, high: 183.01, low: 177.39, close: 179.93 },
-      { time: '2018-11-05', open: 181.02, high: 182.41, low: 179.30, close: 182.19 }
-  ];
-
-  const connectToIB = async() =>{
-    let api = new Client({
-      host: '127.0.0.1',
-      port: 4001,
-    });
-  
-    let time = await api.getCurrentTime();
-    console.log('current time: ' + time);
-  }
-  
+  // variable declartion
   const [stockProperty, setStockProperty] = useState({
-    symbol:"AAPL",
+    symbol:"RELIANCE.BSE",
     interval:"1D",
   })
+
+  const [loading, setLoading] = useState(true)
+
+  // const [stockVolumeData, setStockVolumeData] = useState([]);
+  // const [stockPriceData, setStockPriceData] = useState([]);
+  // const [stockAreaData, setStockAreaData] = useState([]);
+
+  const [marketData, setMarketData] = useState(
+    {
+      price:[],
+      volume:[],
+      area:[],
+    }
+  )
+
+  useEffect(() => {
+      getStockPriceData();
+  },[])
+
+  useEffect(() => {
+    getStockPriceData();
+  },[stockProperty])
+  
+  
 
   const onStockPropertyCondition = (e) =>{
     const {name,value} = e.target
@@ -54,17 +45,54 @@ function App() {
   }
 
   const getStockPriceData = () =>{
-    console.log("stock data",stockProperty);
-    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockProperty.symbol}&interval=${stockProperty.interval}&apikey=demo`).then((result)=>{
+    setLoading(true)
+
+    let vol = [];
+    let arr = [];
+    let mov = [];
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockProperty.symbol}&outputsize=full&apikey=A3QPG0GAAYX8VGI2`;
+    axios.get(API_Call).then((result)=>{
       console.log("result", result.data)
-    })
-
+      let time = result.data["Time Series (Daily)"];
+      let co = 1;
+      let avr = 0;
+      for (let key in time) {
+        if (!time.hasOwnProperty(key)) continue;
+        let aa = {
+          time: key,
+          open: parseInt(time[key]["1. open"], 10),
+          high: parseInt(time[key]["2. high"], 10),
+          low: parseInt(time[key]["3. low"], 10),
+          close: parseInt(time[key]["4. close"], 10)
+        };
+        let closePrice = parseInt(time[key]["4. close"], 10);
+        avr = (avr * (co - 1) + closePrice) / co;
+        let cc = {
+          time: key,
+          value: avr
+        };
+        let bb = {
+          time: key,
+          value: parseInt(time[key]["6. volume"], 10)/3000
+        };
+        co++;
+        vol.push(bb);
+        arr.push(aa);
+        mov.push(cc);
+      }
+      setMarketData({
+        price:arr.reverse(),
+        volume:vol.reverse(),
+        area:mov.reverse()
+      })
+      setLoading(false)
+    });
   }
-
+  
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className='row'>
-        <div className='col-md-8'>
+        <div className='col-md-8 tradingview-panel'>
             <h4 className='text-center'>Tradingview</h4>
             <div className='d-flex justify-content-between'>
               <div>
@@ -72,8 +100,6 @@ function App() {
               </div>
               <div>
                 <select className='form-control' name='interval' onChange={onStockPropertyCondition}>
-                  <option value={"1min"}>1min</option>
-                  <option value={"5min"}>5min</option>
                   <option value={"1D"}>1D</option>
                   <option value={"1W"}>1W</option>
                   <option value={"1M"}>1M</option>
@@ -84,11 +110,14 @@ function App() {
               </div>
             </div>
             <div className="w-100 mt-3">
-              <TradingViewChart></TradingViewChart>
+              {!loading && <TradingViewChart propsMarketData = {marketData}/>}
             </div>
         </div>
-        <div className='col-md-4'>
+        <div className='col-md-4 setting-panel'>
             <h4 className='text-center'>Setting</h4>
+            <form>
+              <div className='form-group'></div>
+            </form>
         </div>
       </div>
     </div>
--- src/components/tradingviewchart.js
+++ src/components/tradingviewchart.js
@@ -1,215 +1,240 @@
 import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { createChart, CrosshairMode } from "lightweight-charts";
-//import { priceData } from "./priceData";
-//import { areaData } from './areaData';
-//import { volumeData } from "./volumeData";
 
-const TradingViewChart = () =>{
+const TradingViewChart = (propsMarketData) => {
   const chartContainerRef = useRef();
   const chart = useRef();
   const resizeObserver = useRef();
 
   useEffect(() => {
-    const arr = [];
-    const vol = [];
-    const mov = [];
-    //var str={"time":'2022-1-1',"open":50,"high":45,"low":34,"close":34};
-    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=RELIANCE.BSE&outputsize=full&apikey=A3QPG0GAAYX8VGI2`;
-    const fetchData = async () => {
-      await fetch(API_Call)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          let time = data["Time Series (Daily)"];
-          console.log(data);
-          var co = 1;
-          var avr = 0;
-          for (var key in time) {
-            if (!time.hasOwnProperty(key)) continue;
-            var aa = {
-              time: key,
-              open: parseInt(time[key]["1. open"], 10),
-              high: parseInt(time[key]["2. high"], 10),
-              low: parseInt(time[key]["3. low"], 10),
-              close: parseInt(time[key]["4. close"], 10)
-            };
-            var closePrice = parseInt(time[key]["4. close"], 10);
-            avr = (avr * (co - 1) + closePrice) / co;
-            var cc = {
-              time: key,
-              value: avr
-            };
-            var bb = {
-              time: key,
-              value: parseInt(time[key]["5. volume"], 10)
-            };
-            co++;
-            vol.push(bb);
-            arr.push(aa);
-            mov.push(cc);
-          }
-
-          vol.reverse();
-          arr.reverse();
-          mov.reverse();
-          console.log("2");
-          chart.current = createChart(chartContainerRef.current, {
-            width: chartContainerRef.current.clientWidth,
-            height: chartContainerRef.current.clientHeight,
-            layout: {
-              backgroundColor: "#253248",
-              textColor: "rgba(255, 255, 255, 0.9)"
-            },
-            grid: {
-              vertLines: {
-                color: "#334158"
-              },
-              horzLines: {
-                color: "#334158"
-              }
-            },
-            crosshair: {
-              mode: CrosshairMode.Normal
-            },
-            priceScale: {
-              borderColor: "#485c7b"
-            },
-            timeScale: {
-              borderColor: "#485c7b"
-            }
-          });
-
-          console.log(chart.current);
-
-          const candleSeries = chart.current.addCandlestickSeries({
-            upColor: "#4bffb5",
-            downColor: "#ff4976",
-            borderDownColor: "#ff4976",
-            borderUpColor: "#4bffb5",
-            wickDownColor: "#838ca1",
-            wickUpColor: "#838ca1"
-          });
-          console.log("3");
-          candleSeries.setData(arr);
-
-          const areaSeries = chart.current.addLineSeries({
-            color: "#f48fb1",
-            lineStyle: 0,
-            lineWidth: 1,
-            crosshairMarkerVisible: true,
-            crosshairMarkerRadius: 6,
-            crosshairMarkerBorderColor: "#ffffff",
-            crosshairMarkerBackgroundColor: "#2296f3",
-            lineType: 1,
-            autoscaleInfoProvider: () => ({
-              priceRange: {
-                minValue: 800,
-                maxValue: 2700
-              },
-              margins: {
-                above: 100,
-                below: 100
-              }
-            })
-          });
-
-          areaSeries.setData(mov);
-
-          areaSeries.setMarkers([
-            {
-              time: "2019-04-09",
-              position: "aboveBar",
-              color: "black",
-              shape: "arrowDown"
-            },
-            {
-              time: "2019-05-31",
-              position: "belowBar",
-              color: "red",
-              shape: "arrowUp",
-              id: "id3"
-            },
-            {
-              time: "2019-05-31",
-              position: "belowBar",
-              color: "orange",
-              shape: "arrowUp",
-              id: "id4",
-              text: "example",
-              size: 2
-            }
-          ]);
-
-          chart.current.subscribeCrosshairMove((param) => {
-            // console.log(param.hoveredMarkerId);
-          });
-
-          chart.current.subscribeClick((param) => {
-            //console.log(param.hoveredMarkerId);
-          });
-
-          const priceLine = areaSeries.createPriceLine({
-            price: 2500.0,
-            color: "green",
-            lineWidth: 2,
-
-            axisLabelVisible: true,
-            title: "P/L 500"
-          });
-
-          priceLine.applyOptions({
-            price: 2000.0,
-            color: "red",
-            lineWidth: 3,
-            axisLabelVisible: false,
-            title: "P/L 600"
-          });
-          const coordinate = areaSeries.priceToCoordinate(2500.5);
-          console.log(coordinate);
-          const screenshot = chart.current.takeScreenshot();
-
-          const volumeSeries = chart.current.addHistogramSeries({
-            color: "#182233",
-            lineWidth: 4,
-            priceFormat: {
-              type: "volume"
-            },
-            overlay: true,
-            scaleMargins: {
-              top: 0.8,
-              bottom: 0
-            }
-          });
-          console.log("4");
-          volumeSeries.setData(vol);
-        });
-
-      console.log("1");
-      resizeObserver.current = new ResizeObserver((entries) => {
-        const { width, height } = entries[0].contentRect;
-        chart.current.applyOptions({ width, height });
-        setTimeout(() => {
-          chart.current.timeScale().fitContent();
-        }, 0);
-      });
-
-      resizeObserver.current.observe(chartContainerRef.current);
-
-      return () => resizeObserver.current.disconnect();
+  
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current.clientWidth });
     };
-    fetchData();
-  });
+
+    chart.current = createChart(chartContainerRef.current, {
+      width: chartContainerRef.current.clientWidth,
+      height: 700,
+      layout: {
+        backgroundColor: "#253248",
+        textColor: "rgba(255, 123, 0, 0.9)"
+      },
+      grid: {
+        vertLines: {
+          color: "#334158"
+        },
+        horzLines: {
+          color: "#334158"
+        }
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal
+      },
+      priceScale: {
+        borderColor: "#485c7b"
+      },
+      timeScale: {
+        borderColor: "#485c7b"
+      }
+    });
+
+    const candleSeries = chart.current.addCandlestickSeries({
+      upColor: "#4bffb5",
+      downColor: "#ff4976",
+      borderDownColor: "#ff4976",
+      borderUpColor: "#4bffb5",
+      wickDownColor: "#838ca1",
+      wickUpColor: "#838ca1"
+    });
+
+    const areaSeries = chart.current.addLineSeries({
+      color: "#f48fb1",
+      lineStyle: 0,
+      lineWidth: 1,
+      crosshairMarkerVisible: true,
+      crosshairMarkerRadius: 6,
+      crosshairMarkerBorderColor: "#ffffff",
+      crosshairMarkerBackgroundColor: "#2296f3",
+      lineType: 1,
+      autoscaleInfoProvider: () => ({
+        priceRange: {
+          minValue: 800,
+          maxValue: 2700
+        },
+        margins: {
+          above: 100,
+          below: 100
+        }
+      })
+    });
+
+    candleSeries.setMarkers([
+      {
+        time: "2019-04-09",
+        position: "aboveBar",
+        color: "black",
+        shape: "arrowDown",
+        text: "sell",
+      },
+      {
+        time: "2019-05-31",
+        position: "belowBar",
+        color: "red",
+        shape: "arrowUp",
+        id: "id3"
+      },
+      {
+        time: "2019-05-31",
+        position: "belowBar",
+        color: "orange",
+        shape: "arrowUp",
+        id: "id4",
+        text: "buy",
+        size: 1
+      }
+    ]);
+
+    chart.current.subscribeCrosshairMove((param) => {
+      // console.log(param.hoveredMarkerId);
+    });
+
+    chart.current.subscribeClick((param) => {
+      //console.log(param.hoveredMarkerId);
+    });
+
+    const priceLine = areaSeries.createPriceLine({
+      price: 2500.0,
+      color: "green",
+      lineWidth: 2,
+
+      axisLabelVisible: true,
+      title: "P/L 500"
+    });
+
+    priceLine.applyOptions({
+      price: 2000.0,
+      color: "red",
+      lineWidth: 3,
+      axisLabelVisible: true,
+      title: "P/L 600"
+    });
+    const coordinate = areaSeries.priceToCoordinate(250.50);
+    const screenshot = chart.current.takeScreenshot();
+    const volumeSeries = chart.current.addHistogramSeries({
+      color: "#182233",
+      lineWidth: 4,
+      priceFormat: {
+        type: "volume"
+      },
+      overlay: true,
+      scaleMargins: {
+        top: 0.8,
+        bottom: 0
+      }
+    });
+
+    // 
+    if(propsMarketData.propsMarketData.price.length == 0){
+      candleSeries.setData([]);
+    }else{
+      candleSeries.setData(propsMarketData.propsMarketData.price);
+      areaSeries.setData(propsMarketData.propsMarketData.area)
+      volumeSeries.setData(propsMarketData.propsMarketData.volume);
+    }
+
+    const RSI_D = chart.current.addLineSeries({
+      color: "green",
+      lineStyle: 0,
+      lineWidth: 1,
+      crosshairMarkerVisible: true,
+      crosshairMarkerRadius: 6,
+      crosshairMarkerBorderColor: "#ffffff",
+      crosshairMarkerBackgroundColor: "#2296f3",
+      lineType: 1,
+      autoscaleInfoProvider: () => ({
+        priceRange: {
+          minValue: 800,
+          maxValue: 2700
+        },
+        margins: {
+          above: 100,
+          below: 100
+        }
+      })
+    });
+
+    const RSI_W = chart.current.addLineSeries({
+      color: "blue",
+      lineStyle: 0,
+      lineWidth: 1,
+      crosshairMarkerVisible: true,
+      crosshairMarkerRadius: 6,
+      crosshairMarkerBorderColor: "#ffffff",
+      crosshairMarkerBackgroundColor: "#2296f3",
+      lineType: 1,
+      autoscaleInfoProvider: () => ({
+        priceRange: {
+          minValue: 800,
+          maxValue: 2700
+        },
+        margins: {
+          above: 100,
+          below: 100
+        }
+      })
+    });
+
+    const RSI_M = chart.current.addLineSeries({
+      color: "#f48fb1",
+      lineStyle: 0,
+      lineWidth: 1,
+      crosshairMarkerVisible: true,
+      crosshairMarkerRadius: 6,
+      crosshairMarkerBorderColor: "#ffffff",
+      crosshairMarkerBackgroundColor: "#2296f3",
+      lineType: 1,
+      autoscaleInfoProvider: () => ({
+        priceRange: {
+          minValue: 800,
+          maxValue: 2700
+        },
+        margins: {
+          above: 100,
+          below: 100
+        }
+      })
+    });
+
+
+    // volumeSeries.setData(volumeData);
+    
+    // resizeObserver.current = new ResizeObserver((entries) => {
+    //   const { width, height } = entries[0].contentRect;
+    //   chart.current.applyOptions(800,500);
+      
+    // });
+  
+    // resizeObserver.current.observe(chartContainerRef.current);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      chart.current.remove();
+    }
+    setTimeout(() => {
+      chart.current.timeScale().fitContent();
+    }, 0);
+  },[propsMarketData.propsMarketData]);
 
   // Resize chart on container resizes.
   useEffect(() => {}, []);
 
   return (
-    <div ref={chartContainerRef} className="chart-container" />
+    <div className="App">
+      <div ref={chartContainerRef} className="chart-container" />
+    </div>
   );
 }
 
 export default TradingViewChart
-
 
